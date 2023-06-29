b957e5e1b7fc821c834e7c3fd062e4df649f342d
Revan Kurniawan <rvnkrwn@gmail.com>
2023-06-30 01:24:27 +0700

    change letter to be better

--- server/app/utils/nodemailer.js
+++ server/app/utils/nodemailer.js
@@ -20,8 +20,8 @@ async function sendResetPasswordEmail(fullName, email, token) {
             subject: "Password Reset Request",
             html: `<p>Hello ${fullName},</p>
                 <p>We have received a request to reset the password for your account. To proceed with the password reset process, please click on the link below:</p><br>
-                <p>http://localhost:9000/reset-password/${token}</p>
-                <p>If you did not initiate this request, you can disregard this email. No further action is required.</p><br>
+                <p>Visit: http://localhost:9000/reset-password/${token}</p>
+                <p>If you did not initiate this request, you can disregard this email. No further action is required.</p>
                 <small style="font-style: italic">Noted: Link reset password is valid in 5 minutes</small>
                 <p>Thank you.</p><br>
                 <p>Best regards,</p>
